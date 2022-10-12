7efede225ca4fe9a8a0dbc601c40828c6e76b36c
corderov <valencordero11@gmail.com>
2022-10-12 17:54:14 -0300

    inicio + foro

---
+++ app/foro.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("common.php");
+
+?>
+
+<html>
+	<head>
+		<title>Foro</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+		<link type="text/css" href="styles/style.css" rel="stylesheet"/>
+        
+		<script src="jquery-1.9.1.min.js" type="text/javascript" ></script>
+	</head>
+	<body>
+    <h1>Foro SDS</h1>
+    <div class="container">
+    <div class="jumbotron">
+    <form action="#" method="post">
+        <h2>Ingresa comentario acerca de tu año de cursado en la materia SDS 2022<h2>
+        <textarea name="txtComentario" style="width:600px;height:200px;resize:none;padding:5px;"></textarea>
+        <input name="enviar" type="submit" value="Enviar">
+    </form>
+    </div>
+</div>
+
+
+
+
+
+	
+	</body>
+</html>
--- app/index.php
+++ app/index.php
@@ -2,14 +2,79 @@
 
 require_once("common.php");
 
+$error = "";
+if(isset($_POST['submit'])){
+	if(verificar_usuario($_POST['username'],$_POST['password']) === true){
+		$conjunto_resultados = $connection->query("SELECT * FROM user WHERE userName='{$_POST['username']}'");
+		$resultados = array();
+		$resultados[] = $conjunto_resultados->fetch_assoc();
+		if(!empty($resultados[0])){
+			$_SESSION["id_usuario"] = $resultados[0]["id"];
+			header("Location: foro.php");
+			exit;
+		}
+	}
+	$error = "Usuario incorrecto";
+}
+function verificar_usuario($username, $password){
+	global $connection;
+	$conjunto_resultados = $connection->query("SELECT * FROM user WHERE userName='{$username}'");
+	$resultados = array();
+	$resultados[] = $conjunto_resultados->fetch_assoc();
+	if(!empty($resultados)){
+		$clave_bd = $resultados[0]["password"];
+		if($password == $clave_bd){
+			return true;
+		}
+	}
+	return false;
+}
+
 ?>
 
 <html>
-    <head>
-    <link type="text/css" href="styles/styles.css" rel="stylesheet"/>
-
-    </head>
-    <body>
-        <h1>Work</h1>
-    </body>
+<head>
+<title>Foro</title>
+<!-- Custom Theme files -->
+<link href="styles/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Flat Login Form Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+<!--google fonts-->
+</head>
+<body>
+<!--header start here-->
+<h1>Ingreso a Foro</h1>
+<div class="header agile">
+	<div class="wrap">
+		<div class="login-main wthree">
+			<div class="login">
+			<h3>Iniciar sesión</h3>
+			<form action="#" method="post">
+				<input type="text" placeholder="Usuario" required="" name="username" required>
+				<input type="password" placeholder="Contraseña" name="password" required>
+				<input name="submit" type="submit" value="Ingresar">
+			</form>
+			
+            <?php if(!empty($error)):?>
+                <span><?php echo $error; ?></span>
+		<?php endif;?>
+			
+			
+		</div>
+	</div>
+</div>
+<!--header end here-->
+<!--copy rights end here-->
+<div class="copy-rights w3l">		 	
+	<p>© <?php echo date("Y");?> Seguridad en Desarrollo de Software | XSS </p>		 	
+</div>
+<!--copyrights start here-->
+ 
+</body>
 </html>
