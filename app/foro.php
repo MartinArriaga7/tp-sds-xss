75249a4d9b99223462ac6b593f682e2fdd503520
corderov <valencordero11@gmail.com>
2022-10-21 10:30:50 -0300

    variables en ingles

--- app/foro.php
+++ app/foro.php
@@ -2,21 +2,21 @@
 
 require_once("common.php");
 
-$nombre_usuario = "";
+$user_name = "";
 if(isset($_SESSION["userId"])){
-	$consulta = "SELECT userName FROM user WHERE id = {$_SESSION["userId"]}";
-	$conjunto_resultados = $connection->query($consulta);
-	$resultados = array();
-    $resultados[] = $conjunto_resultados->fetch_assoc();
-	if(isset($resultados[0]) && !empty($resultados[0])){
-		$nombre_usuario = $resultados[0]["userName"];
+	$query = "SELECT userName FROM user WHERE id = {$_SESSION["userId"]}";
+	$result_set = $connection->query($query);
+	$result = array();
+    $result[] = $result_set->fetch_assoc();
+	if(isset($result[0]) && !empty($result[0])){
+		$user_name = $result[0]["userName"];
 	}
 	
 }else{
 	header("Location: index.php");
 	exit;
 }
-$mensaje = "";
+$message = "";
 if(isset($_POST['btnEnviar'])){
 	if(isset($_POST["txtComentario"]) && !empty($_POST["txtComentario"])){
 		global $connection;
@@ -25,22 +25,22 @@ if(isset($_POST['btnEnviar'])){
         // $comment = str_replace(">","&gt", $comment);	
 		$insercion = "INSERT INTO comment (comment, idUsuario) VALUES ('{$comment}',{$_SESSION["userId"]})";
 		if ($connection->query($insercion)) {
-			$mensaje = "Comentario cargado";
+			$message = "Comentario cargado";
 		}else{
 			
-			$mensaje = "Error al tratar de cargar comentario: ".$connection->error;
+			$message = "Error al tratar de cargar comment: ".$connection->error;
 		}
 	}
 }
-$consulta = "SELECT userName as nombre_usuario, comment FROM comment c INNER JOIN  user u ON c.idUsuario = u.id";
-$conjunto_resultados = $connection->query($consulta);
-$resultados = array();
-if ($conjunto_resultados !== false) {
-	for ($i = 0; $i < $conjunto_resultados->num_rows; $i++) {
-		$resultados[] = $conjunto_resultados->fetch_assoc();
+$query = "SELECT userName as user_name, comment FROM comment c INNER JOIN  user u ON c.idUsuario = u.id";
+$result_set = $connection->query($query);
+$result = array();
+if ($result_set !== false) {
+	for ($i = 0; $i < $result_set->num_rows; $i++) {
+		$result[] = $result_set->fetch_assoc();
 	}
 }
-$comentarios = $resultados;
+$comments = $result;
 
 ?>
 
@@ -60,9 +60,10 @@ $comentarios = $resultados;
         
     <div class="row form-group">
         <div class="col" >
-            <?php if(isset($nombre_usuario)):?>
+            <?php if(isset($user_name)):?>
                 <div class="texto">
-                    Usuario logueado: <?php echo $nombre_usuario;?>
+                    Usuario logueado: <?php echo $user_name;?>
+                    &nbsp;&nbsp;
                     <a href="cerrar_sesion.php" color="red">[cerrar sesi&oacuten]</a>
                 </div>
 		    <?php endif;?>
@@ -77,7 +78,7 @@ $comentarios = $resultados;
         <div class="container">
             <div class="row form-group">
                 <div class="col">
-                    <textarea name="txtComentario" class="form-control" id="exampleFormControlTextarea1" placeholder="Ingrese su comentario..." rows="5"></textarea>
+                    <textarea name="txtComentario" class="form-control" id="exampleFormControlTextarea1" placeholder="Ingrese su comment..." rows="5"></textarea>
                 </div>
             
             </div>
@@ -85,8 +86,8 @@ $comentarios = $resultados;
             <div class="row justify-content-between">
 
                 <div class="col-4">
-                    <?php if(!empty($mensaje)):?>
-                        <span><?php echo $mensaje;?></span>
+                    <?php if(!empty($message)):?>
+                        <span><?php echo $message;?></span>
                     <?php endif;?>
                 </div>
                 <div class="col-2">
@@ -100,10 +101,10 @@ $comentarios = $resultados;
 
 <div class="main-body p-0">
 
-<?php if(isset($comentarios) && !empty($comentarios)):?>
+<?php if(isset($comments) && !empty($comments)):?>
     <br/>
     <h2>Comentarios realizados</h2> 
-    <?php foreach($comentarios as $comentario): ?>          
+    <?php foreach($comments as $comment): ?>          
    
             <!-- Forum List -->
             <div class="inner-main-body p-2 p-sm-3 collapse forum-content show">
@@ -112,9 +113,9 @@ $comentarios = $resultados;
                         <div class="media forum-item">
                             <a href="#" data-toggle="collapse" data-target=".forum-content"><img src="https://i.pinimg.com/564x/c4/34/d8/c434d8c366517ca20425bdc9ad8a32de.jpg" class="mr-3 rounded-circle" width="50" alt="User" /></a>
                             <div class="media-body">
-                                <h6><a href="#" data-toggle="collapse" data-target=".forum-content" class="text-body"><?php echo $comentario["nombre_usuario"]; ?></a></h6>
+                                <h6><?php echo $comment["user_name"]; ?></h6>
                                 <p class="text-secondary">
-                                <?php echo $comentario["comment"]; ?>
+                                <?php echo $comment["comment"]; ?>
                                 </p>
                             </div>
                         </div>
