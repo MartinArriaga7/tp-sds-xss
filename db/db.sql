0060504c31f22fd295cbc1648159494fc3bdffe0
MartinArriaga7 <tinchoarriaga77@gmail.com>
2022-10-29 19:47:33 -0300

    fix typo in db script

--- db/db.sql
+++ db/db.sql
@@ -23,4 +23,4 @@ CREATE TABLE IF NOT EXISTS comment (
 
 INSERT INTO user (userName, `password`) VALUES ('sds', 'sds');
 INSERT INTO user (userName, `password`) VALUES ('Valen', '123');
-INSERT INTO user (uderName, `password`) VALUES ('martin', 'martin');
+INSERT INTO user (userName, `password`) VALUES ('martin', 'martin');
